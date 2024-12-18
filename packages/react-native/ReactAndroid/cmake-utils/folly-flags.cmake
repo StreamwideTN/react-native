f125f5a17345ffbc077a7930249da067cefac595
StreamwideTN <139753210+StreamwideTN@users.noreply.github.com>
2024-12-18 16:29:02 +0100

    Update folly-flags.cmake
    Added stack canary protection and FORTIFY

--- packages/react-native/ReactAndroid/cmake-utils/folly-flags.cmake
+++ packages/react-native/ReactAndroid/cmake-utils/folly-flags.cmake
@@ -9,6 +9,17 @@ set(CMAKE_VERBOSE_MAKEFILE on)
 # This CMake file exposes the Folly Flags that all the libraries should use when
 # compiling/linking against a dependency which requires folly.
 
+if (CMAKE_C_COMPILER_ID MATCHES "Clang|GNU")
+    add_compile_options(-fstack-protector-strong)
+    add_link_options(-fstack-protector-strong)
+endif()
+
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -D_FORTIFY_SOURCE=2 -O2 -fPIC")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -D_FORTIFY_SOURCE=2 -O2 -fPIC")
+
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fvisibility=hidden -fno-strict-aliasing")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fvisibility=hidden -fno-strict-aliasing")
+
 SET(folly_FLAGS
         -DFOLLY_NO_CONFIG=1
         -DFOLLY_HAVE_CLOCK_GETTIME=1
